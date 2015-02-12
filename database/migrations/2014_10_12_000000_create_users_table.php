ea18176dcba549d315f83acea39c3a30ae619b7a
rodrigo <lol@lol.lol>
2015-02-12 15:47:23 -0500

    users table upgrade

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,8 @@ class CreateUsersTable extends Migration {
 		{
 			$table->increments('id');
 			$table->string('name');
+			$table->string('username')->unique();
+			$table->string('slug')->unique()->index();
 			$table->string('email')->unique();
 			$table->string('password', 60);
 			$table->rememberToken();
